0c8d839adf53a41cf84a8740f1fca7fcc009e5bf
edoardo de parolis <edodeparolis@gmail.com>
2022-02-21 09:28:46 +0100

    scheletro progetto

---
+++ socialNetwork/src/main/java/com/infedelis/socialNetwork/SocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.infedelis.socialNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkApplication.class, args);
+	}
+
+}
